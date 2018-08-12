7e04f47652320585c5e32cd65441d47f2211a88d
Derex <kodi.tech.help@gmail.com>
2018-08-12 09:57:21 -0700

    .

---
+++ plugin.video.SteadFastTV/resources/modules/user.py
@@ -0,0 +1,9 @@
+import base64 as b
+
+id   = b.b64decode('cGx1Z2luLnZpZGVvLlN0ZWFkRmFzdFRW')
+
+name = b.b64decode('U3RlYWQgRmFzdFRW')
+
+host = b.b64decode('aHR0cDovL2R1YmxpbmlwdHYuZGRucy5uZXQ=')
+
+port = b.b64decode('ODM=')
